ba1a5fe635cda73edeec980bc1c9f9a52cbc3f0c
Jacui Bell <jacuibell@gmail.com>
2024-03-21 22:18:31 -0400

    new

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What is your name? ', (Jacui) => {
+rl.question('What is your name? ', (name) => {
   console.log(`Hello ${Jacui}!`);
   rl.close();
-});
+});let display = document.getElementById('display');
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
