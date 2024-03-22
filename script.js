4d21978c04b9b0d84156ff592daa848302e29dca
jacuib <163212244+jacuib@users.noreply.github.com>
2024-03-21 22:41:57 -0400

    new

--- script.js
+++ script.js
@@ -9,21 +9,4 @@ const rl = readline.createInterface({
 rl.question('What is your name? ', (name) => {
   console.log(`Hello ${Jacui}!`);
   rl.close();
-});let display = document.getElementById('display');
-
-function appendValue(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = '';
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
-    }
-}
-
+});
