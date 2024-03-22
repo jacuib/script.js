75ddfb5265d6662f49ed9d346464fe871334698a
jacuib <163212244+jacuib@users.noreply.github.com>
2024-03-21 22:50:51 -0400

    new js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ rl.question('What is your name? ', (name) => {
   console.log(`Hello ${Jacui}!`);
   rl.close();
 });
+let display = document.getElementById('display');
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
